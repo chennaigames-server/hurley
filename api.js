ae40d60a756233761f2f0ae27d6a4d29cfdca3b4
annamalai <45361913+annamalaicoderays@users.noreply.github.com>
2022-12-19 12:28:23 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 /* API MODULES WITH NODE-JS CLUSTERING */
 const Server = require('./classes/class.cluster_master');
 const server = new Server();
-// /* IF TRUE - CLUSTER ENABLED, FALSE - CLUSTER DISABLED  *//
+// /* IF TRUE - CLUSTER ENABLED, FALSE - CLUSTER DISABLED  */
 server.start(false);

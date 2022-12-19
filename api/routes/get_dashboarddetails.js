bb21d4367bd21c26286ae199428846d989c57f6f
shankarcrays <shankar@coderays.com>
2022-12-19 16:41:08 +0530

    Update get_dashboarddetails.js

--- api/routes/get_dashboarddetails.js
+++ api/routes/get_dashboarddetails.js
@@ -31,28 +31,53 @@ router.post('/', async (req, res) => {
                 response = {
                     "status": "S",
                     "msg": "Success",
-                    "response_code": 1,
-                    "coin_balance": 1000,
-                    "profile_details": {
-                        "nickname": "super_surfer",
-                        "avatar": "1",
-                        "xp_level": 1,
-                        "c_xp": 10,
-                        "t_xp": 500,
-                        "p_p": 1,
-                        "d_xp": "10/500 XP",
-                        "edit_nickname": "Y"
-                    },
                     "app_config": {
-                        "f_u": "N",
-                        "m": "N",
-                        "i_d": "N",
-                        "m_t": 0
+                      "f_u": "N",
+                      "m": "N",
+                      "i_d": "N",
+                      "m_t": 0
                     },
-                    "surfer_details": {
-                        "char":"M"
+                    "coin_balance": 1000,
+                    "nickname": "super_surfer",
+                    "asset_details": {
+                      "gender": "M/F",
+                      "board_id": 1,
+                      "char_id": 1,
+                      "avatar": "1",
+                      "xp_level": 1,
+                      "c_xp": 10,
+                      "t_xp": 500,
+                      "p_p": 70,
+                      "d_xp": "10/500 XP",
+                      "char_details": {
+                        "char_id": 1,
+                        "back_attach": 1,
+                        "bottom_attach": 2,
+                        "face_attach": 3,
+                        "glass_attach": 4,
+                        "hair_attach": 5,
+                        "hand_attach": 6,
+                        "head_attach": 7,
+                        "leg_attach": 8,
+                        "maleClaws_attach": 9,
+                        "ornaments_attach": 10,
+                        "topDress_attach": 11,
+                        "attr": {
+                          "agility": 10,
+                          "stamina": 20,
+                          "energy": 100,
+                          "durability": 10,
+                          "speed": 20,
+                          "damage": 0
+                        }
+                      }
+                    },
+                    "referral_details": {
+                      "code": "123ABC",
+                      "r_bonus": 5000,
+                      "is_redeemed": "Y/N"
                     }
-                }
+                  }
 
         /* LOGGER */
         logger.log({
